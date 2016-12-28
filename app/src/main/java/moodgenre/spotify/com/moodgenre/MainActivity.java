e1d4df693612b41744de430af5ff6ce3793d342e
Charlie Collins <charlescollins@spotify.com>
2016-12-28 18:01:54 -0500

    Add EasyImage and start with select/snap image.

--- app/src/main/java/moodgenre/spotify/com/moodgenre/MainActivity.java
+++ app/src/main/java/moodgenre/spotify/com/moodgenre/MainActivity.java
@@ -1,9 +1,16 @@
 package moodgenre.spotify.com.moodgenre;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
@@ -23,12 +30,25 @@ import com.spotify.sdk.android.player.PlayerEvent;
 import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.SpotifyPlayer;
 
+import java.io.File;
 import java.util.List;
+
+import pl.aprilapps.easyphotopicker.DefaultCallback;
+import pl.aprilapps.easyphotopicker.EasyImage;
+import pl.aprilapps.easyphotopicker.EasyImageConfig;
+import pl.tajchert.nammu.Nammu;
+import pl.tajchert.nammu.PermissionCallback;
+
+import static android.R.attr.data;
 
 public class MainActivity extends Activity implements
         SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
 
-    private static final int REQUEST_CODE = 1738;
+    private static final int SPOTIFY_AUTH_REQUEST_CODE = 1738;
+
+    private Button toggleAuthButton;
+    private Button chooseImageButton;
+    private TextView label1;
 
     private Player mPlayer;
 
@@ -36,6 +56,49 @@ public class MainActivity extends Activity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toggleAuthButton = (Button) findViewById(R.id.button_toggle_spotify_auth);
+        chooseImageButton = (Button) findViewById(R.id.button_choose_image);
+        label1 = (TextView) findViewById(R.id.textView1);
+
+        toggleAuthButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
+            }
+        });
+        chooseImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ///Intent intent = new Intent(Intent.ACTION_PICK,
+                ///        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                ///startActivityForResult(intent, IMAGE_PICKER_REQUEST_CODE);
+
+                EasyImage.openGallery(MainActivity.this, EasyImageConfig.REQ_PICK_PICTURE_FROM_GALLERY);
+            }
+        });
+        chooseImageButton.setEnabled(false);
+
+        // permissions helper
+        Nammu.init(this);
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        Log.d(Constants.TAG, "permission check:" + permissionCheck);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            Nammu.askForPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, new PermissionCallback() {
+                @Override
+                public void permissionGranted() {
+                    chooseImageButton.setEnabled(true);
+                }
+
+                @Override
+                public void permissionRefused() {
+                    Toast.makeText(MainActivity.this, "cannot use image selector, permission not granted", Toast.LENGTH_SHORT).show();
+                    chooseImageButton.setEnabled(false);
+                }
+            });
+        } else {
+            chooseImageButton.setEnabled(true);
+        }
 
         Log.d(Constants.TAG, "onCreate");
 
@@ -48,7 +111,7 @@ public class MainActivity extends Activity implements
                         Constants.SPOTIFY_REDIRECT_URI);
         builder.setScopes(new String[]{"user-read-private", "streaming"});
         AuthenticationRequest request = builder.build();
-        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+        AuthenticationClient.openLoginActivity(this, SPOTIFY_AUTH_REQUEST_CODE, request);
 
         // AMZN
         // Initialize the Amazon Cognito credentials provider
@@ -90,10 +153,29 @@ public class MainActivity extends Activity implements
 
         Log.d(Constants.TAG, "onActivityResult");
 
-        // Check if result comes from the correct activity
-        if (requestCode == REQUEST_CODE) {
+        // image picker callback
+        if (requestCode == EasyImageConfig.REQ_PICK_PICTURE_FROM_GALLERY) {
+            Log.d(Constants.TAG, "IMAGE_PICKER_REQUEST_CODE match, process response");
 
-            Log.d(Constants.TAG, "REQUEST_CODE match, process response");
+            EasyImage.handleActivityResult(requestCode, resultCode, intent, this, new DefaultCallback() {
+                @Override
+                public void onImagePickerError(Exception e, EasyImage.ImageSource source, int type) {
+                    // TODO error
+                    Log.e(Constants.TAG, "EasyImage error:" + e.getMessage());
+                }
+
+                @Override
+                public void onImagePicked(File imageFile, EasyImage.ImageSource source, int type) {
+                    // TODO picked a file
+                    Log.d(Constants.TAG, "EasyImage picked file" + imageFile.getName());
+                }
+            });
+        }
+
+        // Spotify auth callback
+        if (requestCode == SPOTIFY_AUTH_REQUEST_CODE) {
+
+            Log.d(Constants.TAG, "SPOTIFY_AUTH_REQUEST_CODE match, process response");
 
             AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
 
---
+++ app/src/main/java/moodgenre/spotify/com/moodgenre/RekognitionUtils.java
@@ -0,0 +1,46 @@
+package moodgenre.spotify.com.moodgenre;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.services.rekognition.AmazonRekognitionClient;
+import com.amazonaws.services.rekognition.model.DetectLabelsRequest;
+import com.amazonaws.services.rekognition.model.DetectLabelsResult;
+import com.amazonaws.services.rekognition.model.Image;
+import com.amazonaws.util.IOUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+/**
+ * Created by charliecollins on 12/28/16.
+ */
+
+public class RekognitionUtils {
+
+    // TODO refactor this into an actual util
+    public static void sendBytesExample(AWSCredentials credentials) throws Exception {
+
+        InputStream inputStream = new FileInputStream(new File("inputfile.jpg"));
+        ByteBuffer imageBytes = ByteBuffer.wrap(IOUtils.toByteArray(inputStream));
+
+        DetectLabelsRequest request = new DetectLabelsRequest()
+                .withImage(new Image()
+                        .withBytes(imageBytes))
+                .withMaxLabels(10)
+                .withMinConfidence(77F);
+
+        AmazonRekognitionClient rekognitionClient = new AmazonRekognitionClient(credentials);
+        rekognitionClient.setEndpoint("https://rekognition.us-east-1.amazonaws.com");
+        rekognitionClient.setSignerRegionOverride("us-east-1");
+
+        DetectLabelsResult result = rekognitionClient.detectLabels(request);
+        ObjectMapper objectMapper = new ObjectMapper();
+        System.out.println("Result = " + objectMapper.writeValueAsString(result));
+    }
+
+
+
+
+}
