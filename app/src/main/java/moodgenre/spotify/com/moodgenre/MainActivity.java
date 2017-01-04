33aedd9773cf4aba4aa14521002d17e8835cdfa0
Charlie Collins <charlescollins@spotify.com>
2017-01-04 12:10:04 -0500

    Refactor MainActivity to separate operations into private methods, out of android lifecycle methods and callbacks.

--- app/src/main/java/moodgenre/spotify/com/moodgenre/MainActivity.java
+++ app/src/main/java/moodgenre/spotify/com/moodgenre/MainActivity.java
@@ -24,6 +24,8 @@ import com.spotify.sdk.android.authentication.AuthenticationResponse;
 import com.spotify.sdk.android.player.Config;
 import com.spotify.sdk.android.player.ConnectionStateCallback;
 import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Metadata;
+import com.spotify.sdk.android.player.PlaybackState;
 import com.spotify.sdk.android.player.Player;
 import com.spotify.sdk.android.player.PlayerEvent;
 
@@ -39,26 +41,31 @@ import pl.aprilapps.easyphotopicker.EasyImageConfig;
 import pl.tajchert.nammu.Nammu;
 import pl.tajchert.nammu.PermissionCallback;
 
-import static android.R.attr.data;
 
-public class MainActivity extends Activity implements
-        SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
+public class MainActivity extends Activity  {
 
     private static final int SPOTIFY_AUTH_REQUEST_CODE = 1738;
 
     private Button toggleAuthButton;
     private Button chooseImageButton;
+    private Button playPauseButton;
     private TextView label1;
 
-    private Player mPlayer;
+    private Player spotifyPlayer;
+    private ConnectionStateCallback spotifyConnectionStateCallback;
+    private Player.NotificationCallback spotifyPlayerNotificationCallback;
+    private Player.OperationCallback spotifyPlayerOperationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d(Constants.TAG, "onCreate");
+
         toggleAuthButton = (Button) findViewById(R.id.button_toggle_spotify_auth);
         chooseImageButton = (Button) findViewById(R.id.button_choose_image);
+        playPauseButton = (Button) findViewById(R.id.button_play_pause);
         label1 = (TextView) findViewById(R.id.textView1);
 
         toggleAuthButton.setOnClickListener(new View.OnClickListener() {
@@ -67,84 +74,109 @@ public class MainActivity extends Activity implements
                 Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
             }
         });
+
         chooseImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ///Intent intent = new Intent(Intent.ACTION_PICK,
                 ///        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 ///startActivityForResult(intent, IMAGE_PICKER_REQUEST_CODE);
-
                 EasyImage.openGallery(MainActivity.this, EasyImageConfig.REQ_PICK_PICTURE_FROM_GALLERY);
             }
         });
         chooseImageButton.setEnabled(false);
 
-        // permissions helper
-        Nammu.init(this);
-        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        Log.d(Constants.TAG, "permission check:" + permissionCheck);
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            Nammu.askForPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, new PermissionCallback() {
-                @Override
-                public void permissionGranted() {
-                    chooseImageButton.setEnabled(true);
-                }
-
-                @Override
-                public void permissionRefused() {
-                    Toast.makeText(MainActivity.this, "cannot use image selector, permission not granted", Toast.LENGTH_SHORT).show();
-                    chooseImageButton.setEnabled(false);
-                }
-            });
-        } else {
-            chooseImageButton.setEnabled(true);
-        }
-
-        Log.d(Constants.TAG, "onCreate");
-
-        // TODO check if already authed?
-        // Spotify auth
-        AuthenticationRequest.Builder builder =
-                new AuthenticationRequest.Builder(
-                        Constants.SPOTIFY_CLIENT_ID,
-                        AuthenticationResponse.Type.TOKEN,
-                        Constants.SPOTIFY_REDIRECT_URI);
-        builder.setScopes(new String[]{"user-read-private", "streaming"});
-        AuthenticationRequest request = builder.build();
-        AuthenticationClient.openLoginActivity(this, SPOTIFY_AUTH_REQUEST_CODE, request);
-
-        // AMZN
-        // Initialize the Amazon Cognito credentials provider
-        final CognitoCachingCredentialsProvider credentialsProvider =
-                new CognitoCachingCredentialsProvider(getApplicationContext(),
-                Constants.AMZN_IDENTITY_POOL_ID,
-                Regions.US_EAST_2
-        );
-        new Thread(new Runnable() {
-            public void run() {
-                String identityId = credentialsProvider.getIdentityId();
-                Log.d(Constants.TAG, "AMZN ID is " + identityId);
-            }
-        }).start();
-
-        /*
-        // Initialize the Cognito Sync client
-        CognitoSyncManager syncClient = new CognitoSyncManager(
-                getApplicationContext(),
-                Regions.US_EAST_2, // Region
-                credentialsProvider);
-        // Create a record in a dataset and synchronize with the server
-        Dataset dataset = syncClient.openOrCreateDataset("myDataset");
-        dataset.put("myKey", "myValue");
-        dataset.synchronize(new DefaultSyncCallback() {
+        playPauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(Dataset dataset, List newRecords) {
-                //Your handler code here
+            public void onClick(View v) {
+
+                // toggle spot pause/play
+
+                if (spotifyPlayer == null) {
+                    Toast.makeText(MainActivity.this, "Spotify player not ready", Toast.LENGTH_SHORT).show();
+                } else {
+                    // SpotifyPlayer provides init and connect methods; direct controls (play uri, pause, seek, skip, resume); and state (metadata and playbackstate)
+                    PlaybackState playbackState = spotifyPlayer.getPlaybackState();
+                    Metadata metadata = spotifyPlayer.getMetadata();
+                    if (playbackState.isPlaying) {
+                        spotifyPlayer.pause(spotifyPlayerOperationCallback);
+                    } else {
+
+                        if (playbackState.positionMs > 0) {
+                            // TODO how to tell if player is paused, just position > 0? or is there an actual pause state?
+                            spotifyPlayer.resume(spotifyPlayerOperationCallback);
+                        } else {
+                            // start the track
+                            spotifyPlayer.playUri(null, "spotify:track:7BKLCZ1jbUBVqRi2FVlTVw", 0, 0);
+                        }
+                    }
+                }
             }
         });
-        */
 
-        Log.d(Constants.TAG, "AuthenticationClient LoginActivity opened");
+        spotifyPlayerNotificationCallback = new Player.NotificationCallback() {
+            @Override
+            public void onPlaybackEvent(PlayerEvent playerEvent) {
+                Log.d(Constants.TAG, "Spotify player notif callback: playback event received: " + playerEvent.name());
+                handleSpotifyEvent(playerEvent);
+            }
+
+            @Override
+            public void onPlaybackError(Error error) {
+                Log.d(Constants.TAG, "Spotify player notif callback: playback error received: " + error.name());
+                handleSpotifyError(error);
+            }
+        };
+
+        // spotify player callback
+        spotifyConnectionStateCallback = new ConnectionStateCallback() {
+
+            @Override
+            public void onLoggedIn() {
+                Log.d(Constants.TAG, "Spotify connection callback: User logged in");
+            }
+
+            @Override
+            public void onLoggedOut() {
+                Log.d(Constants.TAG, "Spotify connection callback: user logged out");
+            }
+
+            @Override
+            public void onLoginFailed(Error e) {
+                Log.d(Constants.TAG, "Spotify connection callback: login failed: " + e.toString());
+            }
+
+            @Override
+            public void onTemporaryError() {
+                Log.d(Constants.TAG, "Spotify connection callback: temp error occurred");
+            }
+
+            @Override
+            public void onConnectionMessage(String message) {
+                Log.d(Constants.TAG, "Spotify connection callback: connection message: " + message);
+            }
+        };
+
+        spotifyPlayerOperationCallback = new Player.OperationCallback() {
+            @Override
+            public void onSuccess() {
+                Log.d(Constants.TAG, "Spotify operation callback: success");
+            }
+
+            @Override
+            public void onError(Error error) {
+                Log.d(Constants.TAG, "Spotify operation callback: error " + error.name());
+                Toast.makeText(MainActivity.this, "Spotify op error: " + error.name(), Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        initFromOnCreate();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
     }
 
     @Override
@@ -174,83 +206,175 @@ public class MainActivity extends Activity implements
 
         // Spotify auth callback
         if (requestCode == SPOTIFY_AUTH_REQUEST_CODE) {
-
             Log.d(Constants.TAG, "SPOTIFY_AUTH_REQUEST_CODE match, process response");
 
             AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
-
             Log.d(Constants.TAG, "response: " + response.toString());
 
             if (response.getType() == AuthenticationResponse.Type.TOKEN) {
-                Config playerConfig = new Config(this, response.getAccessToken(), Constants.SPOTIFY_CLIENT_ID);
-                Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
-                    @Override
-                    public void onInitialized(SpotifyPlayer spotifyPlayer) {
-                        mPlayer = spotifyPlayer;
-                        mPlayer.addConnectionStateCallback(MainActivity.this);
-                        mPlayer.addNotificationCallback(MainActivity.this);
-                    }
-
-                    @Override
-                    public void onError(Throwable throwable) {
-                        Log.e(Constants.TAG, "Could not initialize player: " + throwable.getMessage());
-                    }
-                });
+                initSpotifyPlayer(response.getAccessToken());
             }
         }
     }
 
-    @Override
-    protected void onDestroy() {
-        Spotify.destroyPlayer(this);
-        super.onDestroy();
+    //
+    // private
+    //
+
+    private void initFromOnCreate() {
+        checkPerms();
+
+        initAmazonAuth();
+
+        initSpotifyAuth();
     }
 
-    @Override
-    public void onPlaybackEvent(PlayerEvent playerEvent) {
-        Log.d(Constants.TAG, "Playback event received: " + playerEvent.name());
+    private void checkPerms() {
+        // NAMMU permissions helper
+        Nammu.init(this);
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        Log.d(Constants.TAG, "permission check:" + permissionCheck);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            Nammu.askForPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, new PermissionCallback() {
+                @Override
+                public void permissionGranted() {
+                    chooseImageButton.setEnabled(true);
+                }
+
+                @Override
+                public void permissionRefused() {
+                    Toast.makeText(MainActivity.this, "cannot use image selector, permission not granted", Toast.LENGTH_SHORT).show();
+                    chooseImageButton.setEnabled(false);
+                }
+            });
+        } else {
+            chooseImageButton.setEnabled(true);
+        }
+    }
+
+    //
+    // AMZN
+    //
+
+    private void initAmazonAuth() {
+        // AMZN
+        // Initialize the Amazon Cognito credentials provider
+        final CognitoCachingCredentialsProvider credentialsProvider =
+                new CognitoCachingCredentialsProvider(getApplicationContext(),
+                        Constants.AMZN_IDENTITY_POOL_ID,
+                        Regions.US_EAST_2
+                );
+        new Thread(new Runnable() {
+            public void run() {
+                String identityId = credentialsProvider.getIdentityId();
+                Log.d(Constants.TAG, "AMZN ID is " + identityId);
+            }
+        }).start();
+
+        /*
+        // Initialize the Cognito Sync client
+        CognitoSyncManager syncClient = new CognitoSyncManager(
+                getApplicationContext(),
+                Regions.US_EAST_2, // Region
+                credentialsProvider);
+        // Create a record in a dataset and synchronize with the server
+        Dataset dataset = syncClient.openOrCreateDataset("myDataset");
+        dataset.put("myKey", "myValue");
+        dataset.synchronize(new DefaultSyncCallback() {
+            @Override
+            public void onSuccess(Dataset dataset, List newRecords) {
+                //Your handler code here
+            }
+        });
+        */
+    }
+
+    //
+    // SPOT
+    //
+
+    private void initSpotifyAuth() {
+        // TODO check if already authed?
+        AuthenticationRequest.Builder builder =
+                new AuthenticationRequest.Builder(
+                        Constants.SPOTIFY_CLIENT_ID,
+                        AuthenticationResponse.Type.TOKEN,
+                        Constants.SPOTIFY_REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, SPOTIFY_AUTH_REQUEST_CODE, request);
+    }
+
+    private void initSpotifyPlayer(String oauthToken) {
+        Config playerConfig = new Config(this, oauthToken, Constants.SPOTIFY_CLIENT_ID);
+        Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
+            @Override
+            public void onInitialized(SpotifyPlayer player) {
+                spotifyPlayer = player;
+                spotifyPlayer.addConnectionStateCallback(spotifyConnectionStateCallback);
+                spotifyPlayer.addNotificationCallback(spotifyPlayerNotificationCallback);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                Log.e(Constants.TAG, "Could not initialize player: " + throwable.getMessage());
+            }
+        });
+    }
+
+    private void handleSpotifyEvent(final PlayerEvent playerEvent) {
+        Log.d(Constants.TAG, "Spotify playerEvent:" + playerEvent.name());
+
         switch (playerEvent) {
-            // Handle event type as necessary
+
+            case kSpPlaybackEventAudioFlush:
+                break;
+            case kSpPlaybackNotifyAudioDeliveryDone:
+                break;
+            case kSpPlaybackNotifyBecameActive:
+                break;
+            case kSpPlaybackNotifyBecameInactive:
+                break;
+            case kSpPlaybackNotifyContextChanged:
+                break;
+            case kSpPlaybackNotifyLostPermission:
+                break;
+            case kSpPlaybackNotifyMetadataChanged:
+                break;
+            case kSpPlaybackNotifyNext:
+                break;
+            case kSpPlaybackNotifyPause:
+                break;
+            case kSpPlaybackNotifyPlay:
+                break;
+            case kSpPlaybackNotifyPrev:
+                break;
+            case kSpPlaybackNotifyRepeatOff:
+                break;
+            case kSpPlaybackNotifyRepeatOn:
+                break;
+            case kSpPlaybackNotifyShuffleOff:
+                break;
+            case kSpPlaybackNotifyShuffleOn:
+                break;
+            case kSpPlaybackNotifyTrackChanged:
+                break;
+            case kSpPlaybackNotifyTrackDelivered:
+                break;
+
             default:
                 break;
         }
     }
 
-    @Override
-    public void onPlaybackError(Error error) {
-        Log.d(Constants.TAG, "Playback error received: " + error.name());
+    private void handleSpotifyError(final Error error) {
+        Log.e(Constants.TAG, "Spotify Error:" + error.name());
+
         switch (error) {
-            // Handle error type as necessary
+            // corrupt track, add is playing, need perms, travel restriction, etc
             default:
                 break;
         }
     }
 
-    @Override
-    public void onLoggedIn() {
-        Log.d(Constants.TAG, "User logged in");
-
-        mPlayer.playUri(null, "spotify:track:7BKLCZ1jbUBVqRi2FVlTVw", 0, 0);
-
-    }
-
-    @Override
-    public void onLoggedOut() {
-        Log.d(Constants.TAG, "User logged out");
-    }
-
-    @Override
-    public void onLoginFailed(Error e) {
-        Log.d(Constants.TAG, "Login failed: " + e.toString());
-    }
-
-    @Override
-    public void onTemporaryError() {
-        Log.d(Constants.TAG, "Temporary error occurred");
-    }
-
-    @Override
-    public void onConnectionMessage(String message) {
-        Log.d(Constants.TAG, "Received connection message: " + message);
-    }
 }
