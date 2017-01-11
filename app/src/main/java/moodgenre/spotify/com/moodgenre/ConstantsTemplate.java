9833a4485153224cb333f3b5fcaebb3ea5f60a0b
Charlie Collins <charlescollins@spotify.com>
2017-01-11 16:11:03 -0500

    add ConstantsTemplate

---
+++ app/src/main/java/moodgenre/spotify/com/moodgenre/ConstantsTemplate.java
@@ -0,0 +1,19 @@
+package moodgenre.spotify.com.moodgenre;
+
+/**
+ * Created by charliecollins on 12/27/16.
+ */
+
+public class ConstantsTemplate {
+
+    // TODO move anything sensitive out of constants and load via config (don't store in vcs)
+
+    public static final String TAG = "com.spotify.moodgenre";
+
+    public static final String SPOTIFY_CLIENT_ID = "APP CLIENT ID HERE";
+    public static final String SPOTIFY_REDIRECT_URI = "APP CALLBACK HERE";
+
+    public static final String AMZN_IDENTITY_POOL_ID = "ID HERE";
+
+    public static boolean DEBUG = true;
+}
